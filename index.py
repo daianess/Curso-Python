cbe3705e737b42f04b443ef70fc718f3c9a8c399
Daiane da S. Santos <daianesadm@outlook.com>
2022-06-15 06:22:42 -0300

    Aula 1
    - Input
    - Print

---
+++ index.py
@@ -0,0 +1,10 @@
+import imp
+
+nome = input('Qual o seu nome? ')
+print('Olá', nome,'prazer em te conhecer!')
+
+
+dia = input('Qual o dia do seu aniversário? ')
+mes = input('Qual o mês do seu aniversário? ')
+ano = input('Qual o anoque você nasceu? ')
+print('Seu nascimento foi em:', dia,'/', mes,'/',ano)
