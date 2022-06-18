8c26c78571b90b66d5dec61ff74bc8add3af13cc
Daiane da S. Santos <daianesadm@outlook.com>
2022-06-18 10:19:23 -0300

    Exercício Python #008

--- exercicios/ex008.py
+++ exercicios/ex008.py
@@ -1,6 +1,6 @@
 # Exercício 008 - Escreva um programa que leia um valor em metros e o exiba convertido em centímetros e milímetros.
 
-metro = float(input('Digite uma distância em metros: '))
-cm = metro * 100
-mm = metro * 1000
-print('O valor de {}m em centímetro é de {:.0f}cm e em milímetro é de {:.0f}mm' .format(metro, cm, mm))
+m = float(input('Digite uma distância em metros: '))
+cm = m * 100
+mm = m * 1000
+print('O valor de {}m em centímetro é de {:.0f}cm e em milímetro é de {:.0f}mm' .format(m, cm, mm))
