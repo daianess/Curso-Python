afb15416354c18d6d8f29ce1412be14efb2c862c
Daiane da S. Santos <daianesadm@outlook.com>
2022-06-18 10:17:27 -0300

    Exercício Python #010

--- exercicios/ex010.py
+++ exercicios/ex010.py
@@ -1,1 +1,4 @@
 # Exercício 010 - Crie um programa que leia quanto dinheiro uma pessoa tem na carteira e mostre quantos dólares ela pode comprar. (Considere US$ 1,00 = R$ 3,27)
+
+n = float(input('Quanto dinheiro você tem na sua carteira? '))
+print('Com o valor de R$ {} você conseguiria comprar US$ {:.2f}' .format(n, (n/3.27)))
