cf13ac834a4499ececbca09581eb5ae953528b2c
Daiane da S. Santos <daianesadm@outlook.com>
2022-06-18 10:23:04 -0300

    Exercício Python #005

--- exercicios/ex005.py
+++ exercicios/ex005.py
@@ -1,9 +1,9 @@
 # Exercício 005 - Faça um programa que leia um número Inteiro e mostre na tela seu sucessor e seu antecessor.
 
-numero = int(input('Digite um número: '))
-s = numero + 1
-a = numero - 1
-print('Analisando o número {}, \nSeu sucessor é {} e \nO antecessor é {}' .format(numero, s, a))
+n = int(input('Digite um número: '))
+s = n + 1
+a = n - 1
+print('Analisando o número {}, \nSeu sucessor é {} e \nO antecessor é {}' .format(n, s, a))
 
 # ============================================== OU ============================================= #
 
