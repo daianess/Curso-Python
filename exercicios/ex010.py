639b1e8b4310238881257b6bdaf4b53ddfb2426a
Daiane da S. Santos <daianesadm@outlook.com>
2022-06-19 04:37:31 -0300

    Exercício Python #010

--- exercicios/ex010.py
+++ exercicios/ex010.py
@@ -2,3 +2,9 @@
 
 n = float(input('Quanto dinheiro você tem na sua carteira? '))
 print('Com o valor de R$ {} você conseguiria comprar US$ {:.2f}' .format(n, (n/3.27)))
+
+# ============================================== OU ============================================= #
+
+real = float(input('Qunato dinheiro você tem na carteira? R$ '))
+dolar = real / 3.27
+print('Com R${:.2f} você pode comprar US${:.2f}' .format(real, dolar))
